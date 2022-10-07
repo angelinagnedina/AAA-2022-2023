c162da060109ba48caadcafbdba79af75e323aa1
Angelina <angelinagnedina@gmail.com>
2022-10-07 20:02:36 +0300

    Внесла исправления после рецензии в lesson_3.py

--- Python_class/lesson_3.py
+++ Python_class/lesson_3.py
@@ -20,9 +20,10 @@ def hierarchy() -> None:
             print(*set(value), sep=', ')
 
 
-def report(for_second_option: bool = True) -> None:
+def report(save_to_file: bool = False) -> None:
     """
-    :param for_second_option: True, если используется для опции 2, иначе False
+    Делает сводный отчёт по департаментам.
+    :param save_to_file: True - сохраняет отчёт в csv-файл, False - выводит в консоль.
     """
     with open('../Files_with_data/Corp_Summary.csv', newline='') as data_csv:
         data = csv.reader(data_csv, delimiter=';')
@@ -37,20 +38,29 @@ def report(for_second_option: bool = True) -> None:
 
         for key, value in departments.items():
             statistics[key] = [len(value), str(min(value)) + '-' + str(max(value)), sum(value)//len(value)]
-            if for_second_option:
+            if not save_to_file:
                 print(f'Название департамента - {key}. Численность - {statistics[key][0]}.',
                       f'Вилка зарплат - {statistics[key][1]}.',
                       f'Средняя зарплата - {statistics[key][-1]}.')
 
-        if not for_second_option:
-            with open('../Files_with_data/file_with_report.csv', 'w', newline='') as result:
-                result_writer = csv.writer(result, delimiter=' ')
-                result_writer.writerow(['Название департамента', 'Численность',
-                                        'Вилка зарплат', 'Средняя зарплата'])
-                for key, value in statistics.items():
-                    result_writer.writerow([key, *value])
+        if save_to_file:
+            save_report(statistics)
 
-                print('Данные лежат в ../Files_with_data/file_with_report.csv')
+
+def save_report(stats: dict) -> None:
+    """
+    Сохраняет сводный отчёт по департаментам в csv-файл.
+    :param stats: словарь, где ключ - название департамента, значение - массив со
+        следующими статистиками: численность, вилка зарплат, средняя зарплата.
+    """
+    with open('../Files_with_data/file_with_report.csv', 'w', newline='') as result:
+        result_writer = csv.writer(result, delimiter=' ')
+        result_writer.writerow(['Название департамента', 'Численность',
+                                'Вилка зарплат', 'Средняя зарплата'])
+        for key, value in stats.items():
+            result_writer.writerow([key, *value])
+
+        print('Данные лежат в ../Files_with_data/file_with_report.csv')
 
 
 if __name__ == '__main__':
@@ -63,7 +73,7 @@ if __name__ == '__main__':
     }
     print('Выберите действие из следующих:')
     print(*[f'{key}. {value}' for key, value in options.items()], sep='\n')
-    selected = int(input())
+    selected = False
 
     while not 0 < selected < 4:
         print('Выберите число от 1 до 3.')
@@ -71,7 +81,9 @@ if __name__ == '__main__':
 
     if selected == 1:
         hierarchy()
+
     if selected == 2:
         report()
+
     if selected == 3:
-        report(False)
+        report(True)
