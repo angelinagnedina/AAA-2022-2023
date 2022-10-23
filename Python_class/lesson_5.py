1f61eae0a9555714e9a11608759ff4d85d505b07
Angelina <angelinagnedina@gmail.com>
2022-10-23 14:51:49 +0300

    Сделала дз после лекции № 2 по классам.

---
+++ Python_class/lesson_5.py
@@ -0,0 +1,72 @@
+import json
+import keyword
+
+
+class JsonToObject:
+    """
+        Class that transforms json-object to nested python objects.
+    """
+    def __init__(self, data: dict):
+        for key, value in data.items():
+            # Check whether key is the keyword in python,
+            # if so - add '_' to key.
+            if keyword.iskeyword(key):
+                key += '_'
+
+            # If value is dictionary - make another object.
+            if isinstance(value, dict):
+                new_obj = JsonToObject(value)
+                setattr(self, key, new_obj)
+            else:
+                setattr(self, key, value)
+
+    def __repr__(self):
+        """
+            In case we have nested dictionaries - they will be represented
+            as instances of JsonToObject with collections of their keys.
+        """
+        keys = list(self.__dict__.keys())
+        return f'<Instance of JsonToObject class with attributes: {", ".join(keys)}.>'
+
+
+class ColorizeMixin:
+    """
+        Mixin class that changes the color of output in console.
+    """
+    def change_color(self, text: str):
+        #
+        color = getattr(self, 'repr_color_code', 35)
+        return f'\033[{str(color)}m{text}'
+
+
+class Advert(ColorizeMixin, JsonToObject):
+    """
+        Class that contains info about an item for advertisement.
+    """
+    repr_color_code = 33
+
+    def __init__(self, data: dict):
+        super().__init__(data)
+        # Set price to 0 in case it wasn't mentioned.
+        self.price = getattr(self, 'price', 0)
+        assert self.price >= 0, 'Price must be non-negative!'
+
+    def __repr__(self):
+        return super().change_color(f'{self.title} | {self.price} ₽')
+
+
+if __name__ == '__main__':
+    corgi_str = """{
+    "title": "Вельш-корги",
+    "price": 1000,
+    "class": "dogs",
+    "location": {
+    "address": "сельское поселение Ельдигинское, поселок санатория Тишково, 25",
+    "another_key": 1
+    }
+    }"""
+
+    corgi = json.loads(corgi_str)
+    corgi_ad = Advert(corgi)
+    print(corgi_ad.location.address)
+    print(corgi_ad)
