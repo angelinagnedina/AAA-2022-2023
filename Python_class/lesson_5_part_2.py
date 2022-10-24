5923d92b9b52d5dac0f7a7e6b20d930e35f134a9
Angelina <angelinagnedina@gmail.com>
2022-10-24 13:50:32 +0300

    Последнее задание с практики по классам № 2

---
+++ Python_class/lesson_5_part_2.py
@@ -0,0 +1,32 @@
+emoji = {
+    'grass': '☘️',
+    'fire': '🔥',
+    'water': '💧',
+    'electric': '⚡️'
+}
+
+
+class EmojiMixin:
+    """
+        Mixin that changes the output in the console in case it contains
+        some keywords.
+    """
+    def __str__(self):
+        text = f'{self.name}/{self.poketype}'
+        for key, value in emoji.items():
+            text = text.replace(key, value)
+        return text
+
+
+class Pokemon(EmojiMixin):
+    def __init__(self, name: str, poketype: str):
+        self.name = name
+        self.poketype = poketype
+
+    def __str__(self):
+        return super().__str__()
+
+
+if __name__ == '__main__':
+    bulbasaur = Pokemon(name='Bulbasaur', poketype='electric')
+    print(bulbasaur)
